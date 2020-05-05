273a113575b179c63a8c0c6d5cfdd9f656cd6c5f
udayanga91 <u.senanayake91@gmail.com>
2020-05-05 22:09:47 +0900

    Spring inizelizer

---
+++ spring5webbapp/src/main/java/guru/springframework/spring5webbapp/Spring5webbappApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webbappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webbappApplication.class, args);
+	}
+
+}
---
+++ spring5webbapp/src/test/java/guru/springframework/spring5webbapp/Spring5webbappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5webbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webbappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
